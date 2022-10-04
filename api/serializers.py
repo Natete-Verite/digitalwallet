5808e621932890688c27192e36047de387016ad1
Natete-Verite <nateteverite@gmail.com>
2022-10-04 20:23:14 +0300

    Created a customers, wallets, accounts, cards, loans, transactions, receipts, and notifications API

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,42 @@
 from rest_framework import serializers
-from walletapp.models import Customer
+from walletapp.models import Customer, Wallet, Account, Card, Transaction, Loan, Receipt, Notification
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Customer
         fields = ('first_name', 'last_name', 'email', 'age', 'address',)
+
+class WalletSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Wallet
+         fields = "__all__"  
+         
+class AccountSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Account
+         fields = "__all__" 
+         
+class CardSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Card
+         fields = "__all__"          
+         
+class LoanSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Loan 
+         fields = "__all__" 
+         
+class TransactionSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Transaction
+         fields = "__all__" 
+         
+class ReceiptSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Receipt
+         fields = "__all__" 
+         
+class NotificationSerializer(serializers.ModelSerializer):        
+     class Meta:
+         model =  Notification
+         fields = "__all__"                                   
--- api/views.py
+++ api/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from walletapp.models import Customer
-from .serializers import CustomerSerializer
+from walletapp.models import Customer, Wallet, Account,Card, Loan, Transaction, Transaction, Receipt, Notification
+from .serializers import CustomerSerializer, ReceiptSerializer, WalletSerializer, AccountSerializer, CardSerializer, LoanSerializer, TransactionSerializer, ReceiptSerializer, NotificationSerializer
+
 
 # Create your views here.
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+    
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+    
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    
+class ReceiptViewSet(viewsets.ModelViewSet):
+    queryset = Receipt.objects.all()
+    serializer_class = ReceiptSerializer
+    
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer                            
     
